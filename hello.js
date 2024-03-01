52bd19fc0f93e2180140db6ac83b980afa3e2940
GavinMoore <gavin.moore2121@gmail.com>
2024-03-01 14:27:05 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,43 @@
-console.log("hello");
+const readline = require("readline")
+
+const r_interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const solver = {
+    solve: function(minutes, numSongs, songLengths) {
+        let timeTaken = 0;
+        while (true) {
+            let shortestSongLength = Math.max();
+            let shortestSongIndex = 0;
+            for (let i = 0; i < numSongs; i++) {
+                if (songLengths[i] < shortestSongLength) {
+                    shortestSongLength = songLengths[i]
+                    shortestSongIndex = i;
+                }
+            }
+
+            if (timeTaken + shortestSongLength <= minutes * 60) {
+                numSongs = numSongs - 1;
+                songLengths.splice(shortestSongIndex, 1);
+                timeTaken = timeTaken + shortestSongLength;
+                shortestSongLength = Math.max()
+            } else return timeTaken;
+        }
+    }
+}
+
+r_interface.once("line", line => {
+    // collect line 1, the preamble data
+    const [minutes, numSongs] = line.split('/ +/').map(Number)
+    let songs;
+    r_interface.on("line", line => {
+        // parse lines of body data
+        songs = line.split('/ +/').map(Number)
+    })
+        .on("close", () => {
+            // all data has been read. run and print solution
+            console.log(solver.solve(minutes, numSongs, songs))
+        })
+})
